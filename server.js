34d4f25ee27d8970d0fc9a91d8705ef96983acab
abhishek bhandari <abhishek bhandari>
2017-07-12 20:22:32 -0700

    First step to CI

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function (req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('index');
+    res.send("Hello World");
 });
 
 app.listen(port, function () {
